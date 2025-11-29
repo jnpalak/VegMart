be138d071dad105be92b701f578903e8eab1d36f
Palak Jain <Palak@YISCINTERN111LT.Yash.com>
2025-11-29 21:06:48 +0530

    Add HibernateUtil

---
+++ src/main/java/com/yash/vegmart/utilities/HibernateUtil.java
@@ -0,0 +1,20 @@
+package com.yash.vegmart.utilities;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static SessionFactory factory;
+
+    static {
+        try {
+            factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Exception e) {
+            System.out.println("Error building SessionFactory: " + e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return factory;
+    }
+}
