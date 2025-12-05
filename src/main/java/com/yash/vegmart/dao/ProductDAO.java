e5f3e0519be1b103cdb21a2d0c83fae7f2373550
Palak Jain <Palak@YISCINTERN111LT.Yash.com>
2025-12-05 14:52:42 +0530

    add update product method in dao

--- src/main/java/com/yash/vegmart/dao/ProductDAO.java
+++ src/main/java/com/yash/vegmart/dao/ProductDAO.java
@@ -3,8 +3,17 @@ package com.yash.vegmart.dao;
 import com.yash.vegmart.entity.Category;
 import com.yash.vegmart.entity.Vegetable;
 
+import java.util.List;
+
 public interface ProductDAO
 {
     Category getcategoryById(int id);
     boolean saveProduct(Vegetable veg);
+    List<Vegetable> getAllProducts();
+
+    List<Vegetable> getProductsByCategory(int categoryId);
+
+    Vegetable getProductById(int id);
+
+    void updateProduct(Vegetable v);
 }
