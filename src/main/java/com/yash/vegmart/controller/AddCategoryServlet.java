d9a796f7c37b232f5ceb972f2b4cb96b9411c56d
Palak Jain <Palak@YISCINTERN111LT.Yash.com>
2025-12-05 15:00:51 +0530

    updated add category servlet

--- src/main/java/com/yash/vegmart/controller/AddCategoryServlet.java
+++ src/main/java/com/yash/vegmart/controller/AddCategoryServlet.java
@@ -5,11 +5,16 @@ import com.yash.vegmart.service.CategoryService;
 import com.yash.vegmart.serviceimpl.CategoryServiceImpl;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
+@MultipartConfig
 @WebServlet("/AddCategoryServlet")
 public class AddCategoryServlet extends HttpServlet {
 
@@ -21,11 +26,32 @@ public class AddCategoryServlet extends HttpServlet {
 
         String name = req.getParameter("catTitle");
         String description = req.getParameter("catDescription");
+        Part part = req.getPart("cImage");
 
+        String fileName=part.getSubmittedFileName();
         Category category = new Category();
         category.setCategoryName(name);
         category.setCategoryDescription(description);
+        category.setcImage(fileName);
 
+        String path= req.getRealPath("img") + File.separator +fileName;
+
+        try {
+            //uplaoding file
+            FileOutputStream fos = new FileOutputStream(path);
+            InputStream is = part.getInputStream();
+
+            //reading data
+            byte[] data = new byte[is.available()];
+            is.read(data);
+            //Writing the data
+            fos.write(data);
+            fos.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
         boolean success = categoryService.addCategory(category);
 
         HttpSession session = req.getSession();
