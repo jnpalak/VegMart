9e61819ccd51e9e72af541492da32464d4429792
Palak Jain <Palak@YISCINTERN111LT.Yash.com>
2025-12-05 14:39:26 +0530

    add delete user method

--- src/main/java/com/yash/vegmart/service/ProductService.java
+++ src/main/java/com/yash/vegmart/service/ProductService.java
@@ -3,9 +3,17 @@ package com.yash.vegmart.service;
 import com.yash.vegmart.entity.Category;
 import com.yash.vegmart.entity.Vegetable;
 
+import java.util.List;
+
 public interface ProductService
 {
     Category getcategoryById(int id);
-
     boolean saveProduct(Vegetable veg);
+    List<Vegetable> getAllProducts();
+    List<Vegetable> getProductsByCategory(int categoryId);
+
+    Vegetable getProductById(int vid);
+
+
+    void updateProduct(Vegetable v);
 }
