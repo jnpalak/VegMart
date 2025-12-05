a2b99f09a49f442999b857a418115dd5ce676568
Palak Jain <Palak@YISCINTERN111LT.Yash.com>
2025-12-05 14:54:38 +0530

    updated logout servlet

--- src/main/java/com/yash/vegmart/controller/LogoutServlet.java
+++ src/main/java/com/yash/vegmart/controller/LogoutServlet.java
@@ -8,8 +8,15 @@ import javax.servlet.http.*;
 public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession httpSession = request.getSession();
-        httpSession.removeAttribute("userObj");
-        response.sendRedirect("login.jsp");
+        HttpSession httpSession = request.getSession(false);
+        if(httpSession!=null) {
+            httpSession.removeAttribute("cart");
+            httpSession.removeAttribute("cartCount");
+            httpSession.removeAttribute("cartTotal");
+            httpSession.removeAttribute("userObj");
+            httpSession.invalidate();
+        }
+        response.sendRedirect("index.jsp");
+
     }
 }
