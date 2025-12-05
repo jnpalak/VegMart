0e6ea87507a94122265593412b21398fbfa5af21
Palak Jain <Palak@YISCINTERN111LT.Yash.com>
2025-12-05 14:59:46 +0530

    updated add to product servlet

--- src/main/java/com/yash/vegmart/controller/AddProductServlet.java
+++ src/main/java/com/yash/vegmart/controller/AddProductServlet.java
@@ -3,12 +3,14 @@ import com.yash.vegmart.entity.Category;
 import com.yash.vegmart.entity.Vegetable;
 import com.yash.vegmart.service.ProductService;
 import com.yash.vegmart.serviceimpl.ProductServiceImpl;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @MultipartConfig
 @WebServlet("/AddProductServlet")
@@ -37,17 +39,41 @@ public class AddProductServlet extends HttpServlet
        veg.setQuantityInStock(pStock);
        veg.setpImage(fileName);
 
+       //product save
        Category category= proService.getcategoryById(pOption);
        veg.setCategory(category);
-       boolean saved = proService.saveProduct(veg);
-       HttpSession session = req.getSession();
 
+       boolean saved = proService.saveProduct(veg);
+
+       //pic upload
+        // find out the path to uplaod image
+        String path= req.getRealPath("img") + File.separator + "Products"+ File.separator +fileName;
+
+        try {
+            //uplaoding file
+            FileOutputStream fos = new FileOutputStream(path);
+            InputStream is = part.getInputStream();
+
+            //reading data
+            byte[] data = new byte[is.available()];
+            is.read(data);
+            //Writing the data
+            fos.write(data);
+            fos.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+       HttpSession session = req.getSession();
         if (saved) {
             session.setAttribute("proMsg", "Product Added Successfully!");
         } else {
             session.setAttribute("proMsg", "Failed To Add Product!");
         }
         resp.sendRedirect("admin.jsp");
+
     }
 
 }
