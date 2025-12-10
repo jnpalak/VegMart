41cb0ff5b46a40f0486d8879fdff5e5a9ee8aa00
Palak Jain <Palak@YISCINTERN111LT.Yash.com>
2025-12-10 16:00:28 +0530

    order service impl class

---
+++ src/main/java/com/yash/vegmart/serviceimpl/OrderServiceImpl.java
@@ -0,0 +1,49 @@
+package com.yash.vegmart.serviceimpl;
+
+import com.yash.vegmart.dao.OrderDAO;
+
+import com.yash.vegmart.dao.ProductDAO;
+
+import com.yash.vegmart.daoimpl.OrderDAOImpl;
+
+import com.yash.vegmart.daoimpl.ProductDAOImpl;
+
+import com.yash.vegmart.entity.*;
+
+        import com.yash.vegmart.service.OrderService;
+
+import java.util.*;
+
+public class OrderServiceImpl implements OrderService {
+
+    private OrderDAO orderDAO = new OrderDAOImpl();
+
+    private ProductDAO productDAO = new ProductDAOImpl();
+
+    @Override
+
+    public void placeOrder(User user, Map<Integer, CartItem> cart, String paymentMode, double totalAmount) {
+        orderDAO.placeOrder(user,cart,paymentMode,totalAmount);
+    }
+    @Override
+    public List<Order> getAllOrders()
+    {
+        return orderDAO.getAllOrders();
+    }
+    @Override
+    public int saveOrder(Order order)
+    {
+        return orderDAO.saveOrder(order);
+    }
+    @Override
+    public List<Order> getOrdersByUser(int userId)
+    {
+        return orderDAO.getOrdersByUser(userId);
+    }
+    @Override
+    public boolean updateOrderStatus(int orderId, String status)
+    {
+        return orderDAO.updateOrderStatus(orderId,status);
+    }
+}
+
