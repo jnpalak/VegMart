c86187ecb0200fdcf77fc25d03f2dd06b5136ce0
Palak Jain <Palak@YISCINTERN111LT.Yash.com>
2025-12-10 15:59:18 +0530

    updated helper class

--- src/main/java/com/yash/vegmart/utilities/Helper.java
+++ src/main/java/com/yash/vegmart/utilities/Helper.java
@@ -4,9 +4,9 @@ public class Helper
 {
     public static String get35Words(String desc) {
         String[] str = desc.split(" ");
-        if (str.length > 35) {
+        if (str.length > 30) {
             StringBuilder res = new StringBuilder();
-            for (int i = 0; i < 35; i++) {
+            for (int i = 0; i < 30; i++) {
                 res.append(str[i]).append(" ");
             }
             return res.toString().trim() + "....";
