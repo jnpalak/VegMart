abdaca987342d7255fdc722ae9fb05d9f487beba
Palak Jain <Palak@YISCINTERN111LT.Yash.com>
2025-12-10 12:34:38 +0530

    updated delete admin servlet

--- src/main/java/com/yash/vegmart/controller/DeleteAdminServlet.java
+++ src/main/java/com/yash/vegmart/controller/DeleteAdminServlet.java
@@ -1,59 +1,32 @@
 package com.yash.vegmart.controller;
-
 import com.yash.vegmart.service.UserService;
-
 import com.yash.vegmart.serviceimpl.UserServiceImpl;
-
 import com.yash.vegmart.entity.User;
-
 import javax.servlet.ServletException;
-
 import javax.servlet.annotation.WebServlet;
-
 import javax.servlet.http.*;
 import java.io.IOException;
 
 @WebServlet("/DeleteAdminServlet")
-
 public class DeleteAdminServlet extends HttpServlet {
-
     @Override
-
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-
             throws ServletException, IOException {
-
         int adminId = Integer.parseInt(req.getParameter("adminId"));
-
         HttpSession session = req.getSession();
-
         UserService userService = new UserServiceImpl();
-
         User admin = userService.getUserById(adminId);
-
         if (admin != null && admin.getUserType().equals("admin")) {
-
             boolean deleted = userService.deleteAdmin(adminId);
-
             if (deleted) {
-
                 session.setAttribute("successAdmin", "Admin deleted successfully!");
-
             } else {
-
                 session.setAttribute("errorAdmin", "Unable to delete admin. Try again.");
-
             }
-
         } else {
-
             session.setAttribute("errorAdmin", "Invalid admin selected!");
-
         }
-
         resp.sendRedirect("addAdmin.jsp");
-
     }
-
 }
 
